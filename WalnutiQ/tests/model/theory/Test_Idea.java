8b5d97a0687485a5731431c8ec50fe0e0a1098ce
Quinn Liu <quinnliu@vt.edu>
2013-07-01 22:01:46 -0400

    Changed test for adding ColumnPositions to always take a new set of ColumnPositions to simulate the new set of ColumnPositions created from the SpatialPooler.

--- WalnutiQ/tests/model/theory/Test_Idea.java
+++ WalnutiQ/tests/model/theory/Test_Idea.java
@@ -17,22 +17,26 @@ public class Test_Idea extends junit.framework.TestCase {
 
     public void test_unionColumns() {
 	ColumnPosition column_00 = new ColumnPosition(0, 0);
-	Set<ColumnPosition> columns = new HashSet<ColumnPosition>();
-	columns.add(column_00);
-	this.idea.unionColumnPositions(columns);
-	assertEquals(1, columns.size());
+	Set<ColumnPosition> columns_1 = new HashSet<ColumnPosition>();
+	columns_1.add(column_00);
+	this.idea.unionColumnPositions(columns_1);
+	assertEquals(1, this.idea.getColumnPositions().size());
 
 	// make column_01 different than column_00 before adding it to idea
+	Set<ColumnPosition> columns_2 = new HashSet<ColumnPosition>();
+	columns_2.add(column_00);
 	ColumnPosition column_01 = new ColumnPosition(0, 1);
-	columns.add(column_01);
-	this.idea.unionColumnPositions(columns);
-	assertEquals(2, columns.size());
+	columns_2.add(column_01);
+	this.idea.unionColumnPositions(columns_2);
+	assertEquals(2, this.idea.getColumnPositions().size());
 
 	// make column_02 the same as column_01 before adding it to idea
+	Set<ColumnPosition> columns_3 = new HashSet<ColumnPosition>();
+	columns_3.add(column_01);
 	ColumnPosition column_02 = new ColumnPosition(0, 1);
-	columns.add(column_02);
-	this.idea.unionColumnPositions(columns);
-	assertEquals(2, columns.size());
+	columns_3.add(column_02);
+	this.idea.unionColumnPositions(columns_3);
+	assertEquals(2, this.idea.getColumnPositions().size());
     }
 
     public void test_HashSetRemoveColumnPosition() {
