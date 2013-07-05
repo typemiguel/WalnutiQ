76f391f8622427238b68109475f25faeb6e3f012
Quinn Liu <quinnliu@vt.edu>
2013-07-05 00:08:20 -0400

    Because a Idea class constructor now takes a Region object, this change needed to be applied to classes that use the Idea class.

--- WalnutiQ/tests/model/theory/Test_Memory.java
+++ WalnutiQ/tests/model/theory/Test_Memory.java
@@ -1,5 +1,6 @@
 package model.theory;
 
+import model.MARK_II.Region;
 import model.MARK_II.ColumnPosition;
 import java.util.Set;
 import java.util.HashSet;
@@ -10,14 +11,16 @@ import java.util.HashSet;
  */
 public class Test_Memory extends junit.framework.TestCase {
     private Memory memory;
+    private Region region;
 
     public void setUp() {
 	this.memory = new Memory();
+	this.region = new Region("region", 8, 8, 1, 50, 1);
     }
 
     public void test_getIdea() {
-	Idea idea_1 = new Idea("love");
-	Idea idea_2 = new Idea("hate");
+	Idea idea_1 = new Idea("love", this.region);
+	Idea idea_2 = new Idea("hate", this.region);
 
 	this.memory.addNewIdea(idea_1);
 	this.memory.addNewIdea(idea_2);
@@ -25,10 +28,10 @@ public class Test_Memory extends junit.framework.TestCase {
     }
 
     public void test_addNewIdea() {
-	Idea idea_1 = new Idea("date");
-	Idea idea_2 = new Idea("date");
+	Idea idea_1 = new Idea("date", this.region);
+	Idea idea_2 = new Idea("date", this.region);
 
-	Idea idea_3 = new Idea("date");
+	Idea idea_3 = new Idea("date", this.region);
 	ColumnPosition column_00 = new ColumnPosition(0, 0);
 	Set<ColumnPosition> columnPositions = new HashSet<ColumnPosition>();
 	columnPositions.add(column_00);
