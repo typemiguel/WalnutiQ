5df0d28ae4b738a50b1eb665d1c48d9af924a3c5
Quinn Liu <quinnliu@vt.edu>
2013-07-21 20:21:34 -0400

    Added hashcode and equals overriden methods.

--- src/model/MARK_II/Cell.java
+++ src/model/MARK_II/Cell.java
@@ -5,13 +5,15 @@ package model.MARK_II;
  *
  * @author Quinn Liu (quinnliu@vt.edu)
  * @author Michael Cogswell (cogswell@vt.edu)
- * @version MARK II | June 8, 2013
+ * @version MARK II | July 20, 2013
  */
 public class Cell {
     protected boolean isActive;
+    protected boolean wasActive;
 
     public Cell() {
 	this.isActive = false;
+	this.wasActive = false;
     }
 
     public boolean getActiveState() {
@@ -20,5 +22,38 @@ public class Cell {
 
     public void setActiveState(boolean isActive) {
 	this.isActive = isActive;
+    }
+
+    public boolean getPreviousActiveState() {
+	return this.wasActive;
+    }
+
+    public void setPreviousActiveState(boolean wasActive) {
+	this.wasActive = wasActive;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (isActive ? 1231 : 1237);
+	result = prime * result + (wasActive ? 1231 : 1237);
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Cell other = (Cell) obj;
+	if (isActive != other.isActive)
+	    return false;
+	if (wasActive != other.wasActive)
+	    return false;
+	return true;
     }
 }
