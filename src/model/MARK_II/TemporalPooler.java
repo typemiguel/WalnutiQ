3a35508b5a6eabcfdfdabd70ed33c244476be5cd
Quinn Liu <quinnliu@vt.edu>
2013-08-03 17:54:16 -0400

    initial tests for TemporalPooler with inference

--- src/model/MARK_II/TemporalPooler.java
+++ src/model/MARK_II/TemporalPooler.java
@@ -38,6 +38,7 @@ public class TemporalPooler extends Pooler {
 		    if (bestSegment != null && bestSegment.getSequenceState()) {
 			bottomUpPredicted = true;
 			neuron.setActiveState(true);
+			System.out.println("single neuron activated");
 		    }
 		}
 	    }
--- tests/model/MARK_II/Test_TemporalPooler.java
+++ tests/model/MARK_II/Test_TemporalPooler.java
@@ -31,29 +31,70 @@ public class Test_TemporalPooler extends junit.framework.TestCase {
 
 	Set<Column> activeColumns = new HashSet<Column>();
 	activeColumns.add(activeColumn_00);
+
+//	this.temporalPooler.computeActiveStateOfAllNeurons(activeColumns);
+//	for (int x = 0; x < columns.length; x++) {
+//	    for (int y = 0; y < columns[0].length; y++) {
+//		for (Neuron neuron : columns[x][y].getNeurons()) {
+//		    if (x == 0 && y == 0) {
+//			// checking if the for loop that sets all of the neurons
+//			// within a Column was reached
+//			assertTrue(neuron.getActiveState());
+//		    } else {
+//			assertFalse(neuron.getActiveState());
+//		    }
+//		}
+//	    }
+//	}
+//	for (Neuron neuron : columns[0][0].getNeurons()) {
+//	    neuron.setActiveState(false);
+//	}
+//
+	Neuron[] neurons = activeColumn_00.getNeurons();
+//	Neuron neuron_0 = neurons[0];
+//
+//	neuron_0.setPreviousPredictingState(true);
+//
+//	DistalSegment distalSegment_1 = new DistalSegment();
+//	distalSegment_1.setPreviousActiveState(true);
+//	neuron_0.addDistalSegment(distalSegment_1);
+//	this.temporalPooler.computeActiveStateOfAllNeurons(activeColumns);
+//	for (int x = 0; x < columns.length; x++) {
+//	    for (int y = 0; y < columns[0].length; y++) {
+//		for (Neuron neuron : columns[x][y].getNeurons()) {
+//		    if (x == 0 && y == 0) {
+//			// checking if the for loop that sets all of the neurons
+//			// within a Column was reached again because the
+//			// bestSegment was not a sequence segment
+//			assertTrue(neuron.getActiveState());
+//		    } else {
+//			assertFalse(neuron.getActiveState());
+//		    }
+//		}
+//	    }
+//	}
+//	for (Neuron neuron : columns[0][0].getNeurons()) {
+//	    neuron.setActiveState(false);
+//	}
+
+	DistalSegment distalSegment_2 = new DistalSegment();
+	distalSegment_2.setPreviousActiveState(true);
+	distalSegment_2.setSequenceState(true);
+	Neuron neuron_1 = neurons[1];
+	neuron_1.addDistalSegment(distalSegment_2);
 	this.temporalPooler.computeActiveStateOfAllNeurons(activeColumns);
 	for (int x = 0; x < columns.length; x++) {
 	    for (int y = 0; y < columns[0].length; y++) {
-		for (Neuron neuron : columns[x][y].getNeurons()) {
-		    if (x == 0 && y == 0) {
-			assertTrue(neuron.getActiveState());
+		Neuron[] neurons2 = columns[x][y].getNeurons();
+		for (int i = 0; i < neurons2.length; i++) {
+		    if (x == 0 && y == 0 && i == 1) {
+			assertTrue(neurons2[i].getActiveState());
 		    } else {
-			assertFalse(neuron.getActiveState());
+			assertFalse(neurons2[i].getActiveState());
 		    }
 		}
 	    }
 	}
-
-	Neuron[] neurons = activeColumn_00.getNeurons();
-	Neuron neuron_0 = neurons[0];
-
-	neuron_0.setPreviousPredictingState(true);
-
-	DistalSegment distalSegment = new DistalSegment();
-	distalSegment.setPreviousActiveState(true);
-	neuron_0.addDistalSegment(distalSegment);
-
-
     }
 
     public void test_computePredictiveStateOfAllNeurons() {
