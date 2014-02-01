b270012c17426a1f120e036a8b1ec1d48fff302f
Quinn Liu <quinnliu@vt.edu>
2014-02-01 13:37:45 -0500

    removed all System.out.println(...) involving Retine class

--- tests/model/RetineTest.java
+++ tests/model/RetineTest.java
@@ -17,27 +17,11 @@ public class RetineTest extends junit.framework.TestCase {
 
     public void test_seeBMPImage() throws IOException {
 	// comparing the entire 66x66 pixel .bmp image is too much so just
-	// verify specific cells
+	// test specific vision cells
 
 	this.retine.seeBMPImage("2.bmp");
 
 	VisionCell[][] visionCells = this.retine.getVisionCells();
-
-	for (int yPixel = 0; yPixel < visionCells[0].length; yPixel++) {
-	    for (int xPixel = 0; xPixel < visionCells.length; xPixel++) {
-		if (visionCells[xPixel][yPixel].getActiveState() == true) {
-		    System.out.print("1");
-		} else {
-		    System.out.print("0");
-		}
-	    }
-	    System.out.print("\n");
-	}
-
-
-	this.retine.seeBMPImage("2.bmp");
-
-	//VisionCell[][] visionCells = this.retine.getVisionCells();
 
 	// line 8 of 2.bmp = 0000000000000000000000000000011111111111...
 	// index =           0123456789012345678901234567890123456789...
