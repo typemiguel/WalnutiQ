b5a6c6e057b7ea6915afdf94373f3f9915010b22
Quinn Liu <quinnliu@vt.edu>
2014-02-02 09:37:30 -0500

    almost done with experiment

--- experiments/model/MARK_I/vision/NoiseInvarianceExperiment.java
+++ experiments/model/MARK_I/vision/NoiseInvarianceExperiment.java
@@ -17,9 +17,9 @@ import model.MARK_I.SpatialPooler;
  * after adding noise to the data.
  *
  * ------------------------------Experiment-------------------------------------
- * Run the spatial pooling algorithm on a Region of 2 different bitmap images.
- * The 2 images are both of the same circle but one of the images has some
- * random noise added on.
+ * Run the spatial pooling algorithm on 3 different bitmap images. The 3 images
+ * are both of the same thing but one of the images has no noise, one image has
+ * some noise, and one image has a lot of noise.
  *
  * ------------------------------Conclusion-------------------------------------
  * The spatial pooling algoithm does simple local computations on the image to
@@ -56,11 +56,20 @@ public class NoiseInvarianceExperiment extends junit.framework.TestCase {
 
 	this.retina.seeBMPImage("2_with_some_noise.bmp");
 	this.spatialPooler.performSpatialPoolingOnRegion();
-	Set<ColumnPosition> columnActivityAfterSeeingImage2_with_noise = this.spatialPooler
+	Set<ColumnPosition> columnActivityAfterSeeingImage2_with_some_noise = this.spatialPooler
 		.getActiveColumnPositions();
 
-	// make sure that all column positions in the set
-	// columnActivityAfterSeeingImage2 and columnActivityAfterSeeingImage2_with_noise
-	// are the same
+	// TODO: even though there is some noise the spatial pooling algorithm
+	// produces the exact same output
+	assertEquals(columnActivityAfterSeeingImage2.size(),
+		columnActivityAfterSeeingImage2_with_some_noise.size());
+
+	this.retina.seeBMPImage("2_with_alot_of_noise.bmp");
+	this.spatialPooler.performSpatialPoolingOnRegion();
+	Set<ColumnPosition> columnActivityAfterSeeingImage2_with_alot_of_noise = this.spatialPooler
+		.getActiveColumnPositions();
+	// TODO: check if column positions are the same
+	assertEquals(14,
+		columnActivityAfterSeeingImage2_with_alot_of_noise.size());
     }
 }
