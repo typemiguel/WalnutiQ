52c342a74ea004131b838e1d441f9edac924f2ea
Quinn Liu <quinnliu@vt.edu>
2014-02-03 14:36:15 -0500

    finished testing Retina

--- tests/model/RetinaTest.java
+++ tests/model/RetinaTest.java
@@ -27,22 +27,20 @@ public class RetinaTest extends junit.framework.TestCase {
 	// ///////// index = 0123456789012345678901234567890123456789...
 	assertFalse(visionCells[7][28].getActiveState());
 
-	// assertTrue(visionCells[7][29].getActiveState());
+	assertTrue(visionCells[7][29].getActiveState());
 
-	// ---------------------------------------------------------------------
-	// this.retina.seeBMPImage("2_with_some_noise.bmp");
+	// make sure vision cell states in retina has changed after looking at a
+	// new image
+
+	this.retina.seeBMPImage("2_with_some_noise.bmp");
 
 	// line 8 of 2_with_some_noise.bmp
 	// ///// = 0000000000000000000001000000011011111111...
 	// index = 0123456789012345678901234567890123456789...
 
-	// assertFalse(visionCells[8][28].getActiveState());
+	assertTrue(visionCells[7][21].getActiveState());
 
-	// assertTrue(visionCells[8][31].getActiveState());
-
-	// (this.retina.getVisionCells());
-
-	// TODO: assert vision cells in retina has changed
+	assertFalse(visionCells[7][31].getActiveState());
     }
 
     private static void printBMPImageToConsole(VisionCell[][] visionCells) {
