39a8a122902506298952fc9f1fce649e07547573
Quinn Liu <quinnliu@vt.edu>
2014-04-13 11:47:50 -0400

    removed all code related to the temporal pooling algorithm and put it on it's own branch temporal_pooling_algorithm

--- src/model/MARK_I/Neuron.java
+++ src/model/MARK_I/Neuron.java
@@ -1,10 +1,5 @@
 package model.MARK_I;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
-
 /**
  * Simulates a cortical pyramidal cell.
  *
@@ -19,118 +14,9 @@ import java.util.HashSet;
  * @version July 22, 2013
  */
 public class Neuron extends Cell {
-    private boolean isPredicting; // biologically equivalent to depolarized Neuron
-    private boolean wasPredicting;
-
-    // human Neurons have up to 10,000 synapses each
-    private Set<DistalSegment> distalSegments;
 
     public Neuron() {
 	super(); // Initializes isActive state
-	this.isPredicting = false;
-	this.wasPredicting = false;
-	this.distalSegments = new HashSet<DistalSegment>();
-    }
-
-    /**
-     * Return a Segment that was active in the previous time step. If more than
-     * one Segment was was active, sequence segments are considered, and finally
-     * if more than one sequence segment that was active in the previous time
-     * step, the segment with the most activity is returned.
-     *
-     * @return A Segment that was active in the previous time step; otherwise
-     *         null.
-     */
-    public DistalSegment getBestPreviousActiveSegment() {
-	List<DistalSegment> previousActiveSegments = new ArrayList<DistalSegment>();
-	List<DistalSegment> previousActiveSequenceSegments = new ArrayList<DistalSegment>();
-
-	for (DistalSegment distalSegment : this.distalSegments) {
-	    if (distalSegment.getPreviousActiveState()
-		    && distalSegment.getSequenceState()) {
-		previousActiveSegments.add(distalSegment);
-		previousActiveSequenceSegments.add(distalSegment);
-	    } else if (distalSegment.getPreviousActiveState()) {
-		previousActiveSegments.add(distalSegment);
-	    }
-	}
-
-	// the most active sequence Segment at t-1
-	if (previousActiveSequenceSegments.size() > 1) {
-	    DistalSegment mostActiveSequenceSegment = previousActiveSequenceSegments
-		    .get(0);
-	    int maximumPreviousActiveSynapses = 0;
-	    for (DistalSegment sequenceSegment : previousActiveSequenceSegments) {
-		int previousActiveSynapses = sequenceSegment
-			.getNumberOfPreviousActiveSynapses();
-		if (maximumPreviousActiveSynapses < previousActiveSynapses) {
-		    maximumPreviousActiveSynapses = previousActiveSynapses;
-		    mostActiveSequenceSegment = sequenceSegment;
-		}
-	    }
-	    return mostActiveSequenceSegment;
-	} else if (previousActiveSequenceSegments.size() == 1) {
-	    return previousActiveSequenceSegments.get(0);
-	} else {
-	    // get the most active nonsequence Segment at t-1
-	    if (previousActiveSegments.size() > 1) {
-		DistalSegment mostActiveSegment = previousActiveSegments.get(0);
-		int maximumPreviousActiveSynapses = 0;
-		for (DistalSegment segment : previousActiveSegments) {
-		    int previousActiveSynapses = segment
-			    .getNumberOfPreviousActiveSynapses();
-		    if (maximumPreviousActiveSynapses < previousActiveSynapses) {
-			maximumPreviousActiveSynapses = previousActiveSynapses;
-			mostActiveSegment = segment;
-		    }
-		}
-		return mostActiveSegment;
-	    } else if (previousActiveSegments.size() == 1) {
-		return previousActiveSegments.get(0);
-	    } else {
-		return null;
-	    }
-	}
-    }
-
-    /**
-     * Sets current active and predicting state to previous active and
-     * predicting state. Then resets current active and predicting state to
-     * false.
-     */
-    public void nextTimeStep() {
-	this.wasActive = this.isActive;
-	this.wasPredicting = this.isPredicting;
-	this.isActive = false;
-	this.isPredicting = false;
-    }
-
-    public boolean getPredictingState() {
-	return this.isPredicting;
-    }
-
-    public void setPredictingState(boolean predictingState) {
-	this.isPredicting = predictingState;
-    }
-
-    public boolean getPreviousPredictingState() {
-	return this.wasPredicting;
-    }
-
-    public void setPreviousPredictingState(boolean previousPredictingState) {
-	this.wasPredicting = previousPredictingState;
-    }
-
-    public Set<DistalSegment> getDistalSegments() {
-	return this.distalSegments;
-    }
-
-    public void addDistalSegment(DistalSegment distalSegment) {
-	if (distalSegment == null) {
-	    throw new IllegalArgumentException(
-		    "distalSegment in neuron class method addDistalSegment cannot be null");
-	}
-	this.distalSegments.add(distalSegment);
     }
 
     @Override
@@ -143,11 +29,6 @@ public class Neuron extends Cell {
 	stringBuilder.append("\n          wasActive: ");
 	stringBuilder.append(this.wasActive);
 	stringBuilder.append("\n       isPredicting: ");
-	stringBuilder.append(this.isPredicting);
-	stringBuilder.append("\n      wasPredicting: ");
-	stringBuilder.append(this.wasPredicting);
-	stringBuilder.append("\n# of DistalSegments: ");
-	stringBuilder.append(this.distalSegments.size());
 	stringBuilder.append("\n===========================");
 	String NeuronInformation = stringBuilder.toString();
 	return NeuronInformation;
