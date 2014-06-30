9e91d75f99a4a3aba5e9b23af2728e55ce47355b
Quinn Liu <quinnliu@vt.edu>
2014-06-30 00:24:52 -0400

    fing finished issue#85

--- src/main/java/model/util/ConvertToValidCode.java
+++ src/main/java/model/util/ConvertToValidCode.java
@@ -7,9 +7,7 @@ public class ConvertToValidCode {
     /**
      * @param int2DArray must contain \n to represent the next line
      */
-    public void printsToConsoleConvertedJavaForInt2DArray(String int2DArray, int numberOfRows, int numberOfColumns) {
-        int indexNotToPutAComma = numberOfColumns - 2;
-
+    public void printsToConsoleConvertedJavaForInt2DArray(String int2DArray) {
         System.out.println("\n= new int[][] {");
         System.out.print("{ ");
         int int2DArrayLength = int2DArray.length();
@@ -18,17 +16,17 @@ public class ConvertToValidCode {
             char nextChar = int2DArray.charAt(i + 1);
 
             if (nextChar == 'E') {
-                System.out.println(" }\n};");
+                System.out.print(currentChar);
+                System.out.print(" }\n};");
                 return;
             } else if (nextChar == '\n') {
-                System.out.print(" },\n{ ");
-                i++;
-            } else if (i == indexNotToPutAComma) {
                 System.out.print(currentChar);
-                indexNotToPutAComma += numberOfColumns + 1; // + 1 for \n
-                //System.out.print("DON'T PRINT COMMA");
+                System.out.print(" },\n{ ");
             } else {
-                System.out.print(currentChar + ",");
+                // skip \n char
+                if (currentChar != '\n') {
+                    System.out.print(currentChar + ",");
+                }
             }
         }
     }
